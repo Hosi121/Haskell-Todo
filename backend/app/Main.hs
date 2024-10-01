ba8ace7aa4553f7db7811323882ee88ef180b547
Hosi121 <takuyakataiwa@icloud.com>
2024-10-01 17:32:15 +0900

    feat: mainを変更

--- backend/app/Main.hs
+++ backend/app/Main.hs
@@ -1,6 +1,11 @@
-module Main (main) where
+module Main where
 
-import Lib
+import Network.Wai.Handler.Warp (run)
+import Servant
+import Server (app)
 
+-- サーバーを起動するエントリーポイント
 main :: IO ()
-main = someFunc
+main = do
+    putStrLn "Starting server on port 8080..."
+    run 8080 app
